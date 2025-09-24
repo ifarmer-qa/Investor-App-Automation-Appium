3a987e83fd68e903a34a1ce74aca6fdc9a9376ab
Mahmudul Islam <mahmud.kuet.ece.2k13@gmail.com>
2025-09-24 14:35:56 +0600

    update login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,10 +9,9 @@ class LogInPage(BasePage):
         self.login_nav = (self.accessibility_id_locator,"Login\nLogin\nTab 4 of 4")
 
         # LogIN page Locators
-        self.email_input = (self.xpath_locator,"//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText[1]")
-        self.pin = (self.xpath_locator,"")
-
-
+        self.email_input = (self.xpath_locator,"//*[@content-desc='Email']/following-sibling::android.widget.EditText[1]")
+        self.pin_input = (self.xpath_locator,"//*[@content-desc='PIN']/following-sibling::android.widget.EditText[1]")
+        self.login_button = (self.accessibility_id_locator,"Login")
 
 
 
@@ -21,9 +20,56 @@ class LogInPage(BasePage):
             nav_login = self.get_element(*self.login_nav)
 
             if nav_login:
+                self.text_log("Login Nav found", "Login Nav")
+                self.take_screenshot("Login Nav found")
                 self.click(nav_login)
+                return True
+            else:
+                self.text_log("Login Nav not found", "Login Nav")
+                self.take_screenshot("Login Nav not found")
+                return False    
+
+        except Exception as e:
+            self.text_log(f"Error:\n{e}", "Error getting Login Nav")
+            return False
 
 
-
-    def login(self, email, pin)
+    def login(self, email, pin):
         try:
+            email_input = self.get_element(*self.email_input)
+            if email_input:
+                self.text_log("Email input found", "Email input")
+                self.take_screenshot("Email input found")
+                self.enter_text(email_input, email)
+                self.take_screenshot("Email entered")
+            else:
+                self.text_log("Email input not found", "Email input")
+                self.take_screenshot("Email input not found")
+                return False
+
+            pin_input = self.get_element(*self.pin_input)
+            if pin_input:
+                self.text_log("PIN input found", "PIN input")
+                self.take_screenshot("PIN input found")
+                self.enter_text(pin_input, pin)
+                self.take_screenshot("PIN entered")
+            else:
+                self.text_log("PIN input not found", "PIN input")
+                self.take_screenshot("PIN input not found")
+                return False
+
+            login_button = self.get_element(*self.login_button)
+            if login_button:
+                self.text_log("Login button found", "Login button")
+                self.click(login_button)
+                self.take_screenshot("Login button clicked")
+                return True
+            else:
+                self.text_log("Login button not found", "Login button")
+                self.take_screenshot("Login button not found")
+                return False
+
+        except Exception as e:
+            self.text_log(f"Error:\n{e}", "Error logging in")
+            return False
+
--- tests/test_suite_01.py
+++ tests/test_suite_01.py
@@ -5,6 +5,7 @@ import allure
 from pages.initial_loading_page import InitialLoadingPage
 from pages.guest_home_page import GuestHomePage
 from pages.home_page import HomePage
+from pages.login_page import LogInPage
 
 
 
@@ -17,6 +18,7 @@ class TestDemoApp:
         cls.driver = driver_setup
         cls.initial_load_page = InitialLoadingPage(cls.driver)
         cls.guest_home_page = GuestHomePage(cls.driver)
+        cls.login_page = LogInPage(cls.driver)
         cls.home_page = HomePage(cls.driver)
 
 
@@ -35,75 +37,20 @@ class TestDemoApp:
             check.is_true(self.initial_load_page.check_dialog(), "No Permission Dialog displayed") 
         with allure.step("Test Initial Modal"):
             check.is_true(self.initial_load_page.check_welcome_popup(), "No Welcome Popup window")
+            
 
-
-    # @allure.severity(allure.severity_level.CRITICAL)
-    # @pytest.mark.e2e
-    # def test_checkout_order(self, test_data):
-
-    #     login_data = test_data["login"]
-    #     checkout_data = test_data["checkout"]
-    #     payment_data = test_data["payment"]
-
-    #     # Step 1: Open Menu and Login
-    #     self.home_page.click_on_open_menu()
-    #     self.home_page.click_on_login_button()
-
-    #     email = login_data["email"]
-    #     password = login_data["password"]
-    #     self.login_page.login_with_valid_email_and_password(email, password)
-
-    #     self.take_screenshot("Login Successful")
-
-    #     # Step 2: Choose Product
-    #     self.home_page.click_on_product1()
-    #     self.product_page.click_on_counter_plus_button()
-    #     self.product_page.click_on_add_to_cart_button()
-
-    #     self.take_screenshot("Product Added to Cart")
-
-    #     # Step 3: View Cart and Proceed to Checkout
-    #     self.product_page.click_on_cart_badge()
-    #     self.cart_page.click_on_proceed_to_checkout_button()
-
-    #     self.take_screenshot("Proceed to Checkout")
-
-    #     # Step 4: Fill Checkout Information
-    #     checkout = checkout_data.copy()
-
-    #     self.checkout_page.fill_checkout_information(**checkout)
-
-    #     self.take_screenshot("Checkout Information Filled")
-
-    #     # Step 5: Fill Payment Information
-    #     payment =  payment_data.copy()
-
-    #     self.payment_page.fill_payment_information(**payment)
-
-    #     self.take_screenshot("Payment Information Filled")
-
-    #     # Step 6: Review Order
-    #     self.payment_page.click_on_review_order_button()
-
-    #     self.take_screenshot("Review Order")
-
-    #     # Step 7: Place Order
-    #     self.review_order_page.click_on_place_order_button()
-
-    #     self.take_screenshot("Place Order")
-
-    #     # Step 8: Continue Shopping
-    #     self.checkout_complete_page.click_on_continue_shopping_button()
-
-    #     self.take_screenshot("Returned to Home")
-
-    #     # Step 9: Verify
-    #     product_text = self.home_page.get_products_text()
-    #     assert product_text == "Products", f"Expected 'Products' but got {product_text}"
-
-
-
-    # @allure.step("Take a screenshot and attach it to Allure report")
-    # def take_screenshot(self, step_name):
-    #     screenshot = self.driver.get_screenshot_as_png()
-    #     allure.attach(screenshot, name=step_name, attachment_type=allure.attachment_type.PNG)
+    @allure.description(
+        "Given       User is on the iFarmer Home as a guest user\n"
+        "When        Click on Login Nav\n"
+        "Then        On the Login Page\n"
+        "When        Enter valid email and pin\n"
+        "And         Click on Login button\n"
+        "Then        On the iFarmer Home as a logged in user\n"
+    )
+    @allure.severity(allure.severity_level.CRITICAL)
+    @pytest.mark.e2e
+    def test_case_02_login_valid_user(self, test_data):
+        with allure.step("Test Navigate to Login from Guest Home Page"):
+            check.is_true(self.login_page.nav_login_from_guest(), "Could not navigate to Login from Guest Home Page") 
+        with allure.step("Test Login with valid user"):
+            check.is_true(self.login_page.login(test_data["login"]["email"], test_data["login"]["pin"]), "Could not login with valid user")
